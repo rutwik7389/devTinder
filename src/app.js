28bd2a33e9654ec7e1f8157595696606c10ee2c5
Rutwik Sindkar <rutwiksindkar07@gmail.com>
2025-11-01 15:06:23 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -3,23 +3,33 @@ const express = require('express');
 const app=express();
 
 
+//ab+c   //abbbbbbbbc
+//use+r   // e is optional
+//a(cc)?d  // cc is optional now 
 
-app.get('/user',(req,res)=>{
-    res.send("Heyyyy")
+
+app.get('/user/:userId/:name/:password',(req,res)=>{
+    console.log(req.params);
+    res.send({firstName:"Rutwik",lastName :"Sindkar"});
+    
 })
 
-app.post('/user',(req,res)=>{
-res.send("oooooo")
-})
+// app.get('/use?r',(req,res)=>{
+//     res.send("Heyyyy")
+// })
+
+// app.post('/user',(req,res)=>{
+// res.send("oooooo")
+// })
 
 
-app.delete('/user',(req,res)=>{
-res.send("deleted")
-})
+// app.delete('/user',(req,res)=>{
+// res.send("deleted")
+// })
 
-app.use('/hello',(req,res)=>{
-    res.send("Hello from serverr")
-});
+// app.use('/hello',(req,res)=>{
+//     res.send("Hello from serverr")
+// });
 
 
 
