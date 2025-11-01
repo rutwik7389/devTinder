613791a3b3a77f7a3d3db421d6efd206be56a615
Rutwik Sindkar <rutwiksindkar07@gmail.com>
2025-11-01 14:49:04 +0530

    exploring  get post delete

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 
 const app=express();
 
-app.use('/hey',(req,res)=>{
-    res.send("Hello from server")
-});
 
+
+app.get('/user',(req,res)=>{
+    res.send("Heyyyy")
+})
+
+app.post('/user',(req,res)=>{
+res.send("oooooo")
+})
+
+
+app.delete('/user',(req,res)=>{
+res.send("deleted")
+})
 
 app.use('/hello',(req,res)=>{
     res.send("Hello from serverr")
